09b9b33be1cb4b4df8a89b5d4764e64cf047f8ef
Jerry <jerryzhang28eclipse@gmail.com>
2019-06-10 04:18:54 +0800

    Secondary Commit

---
+++ android/app/src/main/java/com/example/quickdiv_orbital2019/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.quickdiv_orbital2019;
+
+import android.os.Bundle;
+
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GeneratedPluginRegistrant.registerWith(this);
+    }
+}
