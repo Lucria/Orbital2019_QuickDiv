acacf38513506e46539510dbb6e1ee6aa46caf7c
Jerry <jerryzhang28eclipse@gmail.com>
2019-06-14 07:15:05 +0800

    Removed certain comments

--- lib/main.dart
+++ lib/main.dart
@@ -35,11 +35,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
     void _incrementCounter() {
         setState(() {
-            // This call to setState tells the Flutter framework that something has
-            // changed in this State, which causes it to rerun the build method below
-            // so that the display can reflect the updated values. If we changed
-            // _counter without calling setState(), then the build method would not be
-            // called again, and so nothing would appear to happen.
             _counter++;
         });
     }
@@ -56,23 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 title: Text(widget.title),
             ),
             body: Center(
-                // Center is a layout widget. It takes a single child and positions it
-                // in the middle of the parent.
                 child: Column(
-                    // Column is also layout widget. It takes a list of children and
-                    // arranges them vertically. By default, it sizes itself to fit its
-                    // children horizontally, and tries to be as tall as its parent.
-                    //
-                    // Invoke "debug painting" (press "p" in the console, choose the
-                    // "Toggle Debug Paint" action from the Flutter Inspector in Android
-                    // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-                    // to see the wireframe for each widget.
-                    //
-                    // Column has various properties to control how it sizes itself and
-                    // how it positions its children. Here we use mainAxisAlignment to
-                    // center the children vertically; the main axis here is the vertical
-                    // axis because Columns are vertical (the cross axis would be
-                    // horizontal).
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
                         Text(
@@ -80,6 +59,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         ),
                         Text(
                             '$_counter',
+                            // This code portion defines text style of _counter
                             style: Theme
                                 .of(context)
                                 .textTheme
@@ -90,9 +70,11 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             floatingActionButton: FloatingActionButton(
                 onPressed: _incrementCounter,
+                // Call the relevant method
                 tooltip: 'Increment',
+                // Will be changed to add contact group or one-time usage
                 child: Icon(Icons.add),
-            ), // This trailing comma makes auto-formatting nicer for build methods.
+            ),
         );
     }
 }
