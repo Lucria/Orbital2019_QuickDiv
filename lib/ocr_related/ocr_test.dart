053eecdb9b39df56a397271c6505c36d939fe0ff
Jerry <jerryzhang28eclipse@gmail.com>
2019-06-22 18:31:08 +0800

    [Feat] Added skeleton code for OCR

--- lib/ocr_related/ocr_test.dart
+++ lib/ocr_related/ocr_test.dart
@@ -1,2 +1,26 @@
-import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'package:mlkit/mlkit.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:async';
+
+class DetectionWidget extends StatefulWidget {
+    @override
+    State<StatefulWidget> createState() {
+        return _DetectionWidgetState();
+    }
+}
+
+class _DetectionWidgetState extends State<DetectionWidget> {
+
+    FirebaseVisionTextDetector textDetector = FirebaseVisionTextDetector
+        .instance;
+    List<VisionText> currentTextLabels = <VisionText>[];
+
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+                title: Text("QuickDiv v1.0"),
+            ),
+        );
+    }
+}
