efa377d481da5803635ba68213520e9eece2444f
Lucria <jerryzhang28eclipse@gmail.com>
2019-07-28 01:29:40 +0800

    [Feat] Added backgrounds to contacts page

--- lib/pages/add_user_group.dart
+++ lib/pages/add_user_group.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:contacts_service/contacts_service.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:quickdiv_orbital2019/widget/ui_elements/background.dart';
 import 'package:scoped_model/scoped_model.dart';
 import './create_group_name.dart';
 import '../models/custom_contacts.dart';
@@ -215,6 +216,7 @@ class _AddUserGroupPage extends State<AddUserGroupPage> {
         ),
         body: !_isLoading
             ? Container(
+                decoration: BackgroundImage.myBoxDecoration(),
                 child: Column(
                 children: <Widget>[
                   Padding(
